0be9bcb8c514aecdaac059bb9db790a52ed4f9ae
Kevin Ramos <kevin.ramos@fiserv.com>
2023-11-17 11:27:44 -0500

    added navbar, mock data w/ json request

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import "./css/navButtons.css";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <>
+      {/* <div className="grid text-center lg:max-w-l lg:w-full lg:mb-0 lg:grid-cols-5 lg:text-left"> */}
+      <div className="grid grid-flow-col gap-4  mx-auto mt-6 md:mt-0 auto-cols-auto md:auto-rows-auto md:gap-8 lg:gap-12">
+        <Link
+          href="/home"
+          className="group rounded-lg border border-transparent text-center py-4 transition-colors"
+          rel="noopener noreferrer"
+        >
+          <button>
+            <span>
+              <h2 className={`mb-3 text-2xl font-semibold`}>Home</h2>
+            </span>
+          </button>
+        </Link>
+
+        <Link
+          href="/orders"
+          className="group rounded-lg border border-transparent text-center py-4 transition-colors"
+          rel="noopener noreferrer"
+        >
+          <button>
+            <span>
+              <h2 className={`mb-3 text-2xl font-semibold`}>Orders</h2>
+            </span>
+          </button>
+        </Link>
+        <Link
+          href="/"
+          className="group rounded-lg border border-transparent text-center py-4 transition-colors"
+          rel="noopener noreferrer"
+        >
+          <button>
+            <span>
+              <h2 className={`mb-3 text-2xl font-semibold`}>Customers</h2>
+            </span>
+          </button>
+        </Link>
+        <Link
+          href="/"
+          className="group rounded-lg border border-transparent text-center py-4 transition-colors"
+          rel="noopener noreferrer"
+        >
+          <button>
+            <span>
+              <h2 className={`mb-3 text-2xl font-semibold`}>Products</h2>
+            </span>
+          </button>
+        </Link>
+        <Link
+          href="/"
+          className="group rounded-lg border border-transparent text-center py-4 transition-colors"
+          rel="noopener noreferrer"
+        >
+          <button>
+            <span>
+              <h2 className={`mb-3 text-2xl font-semibold`}>Admin</h2>
+            </span>
+          </button>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ app/home/page.tsx
@@ -0,0 +1,68 @@
+import React, { use } from "react";
+import {
+  Card,
+  CardFooter,
+  CardHeader,
+  CardBody,
+  Image,
+  Button,
+} from "@nextui-org/react";
+
+import getTestUsers from "@/pages/api";
+
+const Home = async () => {
+  const users = await getTestUsers();
+
+  return (
+    <>
+      <div className="mt-12 text-3xl text-center grid py-10 bg-clip-border rounded-xl">
+        <div>Welcome Employee {users[3].name}!</div>
+      </div>
+      <div className="text-3xl text-left grid py-1 bg-clip-border rounded-xl">
+        <div>Employee ID: {users[3].id}</div>
+      </div>
+      <div className="text-3xl text-left grid py-1 bg-clip-border rounded-xl">
+        <div>Full Name: {users[3].name}</div>
+      </div>
+      <div className="text-3xl text-left grid py-1 bg-clip-border rounded-xl">
+        <div>Active Status: Currently Part time</div>
+      </div>
+      <div className="mt-3 text-3xl pl-12 grid bg-stone-500 py-5 bg-clip-border rounded-xl">
+        <div className="mt-12 grid grid-cols-1 gap-3 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+          <div className="text-left relative flex w-40 flex-col rounded-xl">
+            New Order
+          </div>
+        </div>
+
+        <div className="mt-12 grid grid-cols-1 gap-3 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+          {users.map((user) => (
+            <div
+              key={user.id}
+              className="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md"
+            >
+              <div className="p-6">
+                <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                  {user.name}
+                </h5>
+                <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                  {user.email}
+                </p>
+              </div>
+              <div className="p-6 pt-0">
+                <button
+                  data-ripple-light="true"
+                  type="button"
+                  className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                >
+                  Add to Order
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
