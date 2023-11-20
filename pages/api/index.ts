a90104ed8a7527075c50f5d93a07f901de879ceb
Kevin Ramos <kevin.ramos@fiserv.com>
2023-11-20 14:44:51 -0500

    added custome API calls

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -7,19 +7,108 @@ interface User {
   name: string;
   email: string;
   phone: string;
+  website: string;
 }
 
-type Todo = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
+interface Customers {
+  phoneNo: string;
+  name: string;
+  address: string;
+  zipCode: number;
+}
+
+interface CustomerOrders {
+  customer_order: number;
+  phone: string;
+  employeeid: number;
+}
+
+interface Employee {
+  employeeId: number;
+  activeStatus: string;
+  name: string;
+}
+
+interface OrderDetails {
+  order_number: number;
+  customer_order: string;
+  total_price: number;
+  time: Date;
+}
+
+interface OrderList {
+  orderIndex: number;
+  orderNumber: number;
+  productId: number;
+  quantity: number;
+}
+
+interface Product {
+  product_id: number;
+  name: string;
+  price: number;
+}
 
 export async function getTestUsers() {
   const res = await fetch("http://jsonplaceholder.typicode.com/users");
   const users: User[] = await res.json();
   return users;
+}
+
+export class ProductApi {
+  static async getAllProducts() {
+    const res = await fetch("http://localhost:3000/api/products");
+    const Products: Product[] = await res.json();
+    return Products;
+  }
+  static async findProduct(id: number) {
+    const res = await fetch(`http://localhost:3000/api/product/${id}`);
+    const Products: Product[] = await res.json();
+    return Products;
+  }
+}
+
+export class EmployeeApi {
+  static async getAllEmployees() {
+    const res = await fetch("http://localhost:3000/api/employee");
+    const Employees: Employee[] = await res.json();
+    return Employees;
+  }
+
+  static async findEmployee(id: number) {
+    const res = await fetch(`http://localhost:3000/api/getEmployee/${id}`);
+    const Employee: Employee = await res.json();
+    return Employee;
+  }
+}
+
+export class OrderListApi {
+  static async getAllOrderLists() {
+    const res = await fetch("http://localhost:3000/api/orderList");
+    const OrderList: OrderList[] = await res.json();
+    return OrderList;
+  }
+
+  static async findOrderList(id: number) {
+    const res = await fetch(`http://localhost:3000/api/getOrderList/${id}`);
+    const OrderList: OrderList[] = await res.json();
+    return OrderList;
+  }
+}
+
+
+export class CustomerApi {
+  static async getAllCustomers() {
+    const res = await fetch("http://localhost:3000/api/customers");
+    const Customers: Customers[] = await res.json();
+    return Customers;
+  }
+  
+  static async findCustomer(id: number) {
+    const res = await fetch(`http://localhost:3000/api/getCustomer/${id}`);
+    const Customers: Customers[] = await res.json();
+    return Customers;
+  }
 }
 
 // export async function kevin(request: Request) {
