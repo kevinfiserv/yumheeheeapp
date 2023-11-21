0e10809567ff2a1d329efa135c90d81d21fbefc6
elana.danseglio <elana.danseglio@fiserv.com>
2023-11-21 10:22:18 -0500

    navbar fixed
    Co-authored-by: hanschandler-vidaure <hanschandler-vidaure@users.noreply.github.com>

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,55 @@ import React from "react";
 import "./css/navButtons.css";
 import Link from "next/link";
 
+const NavigationBar: React.FC = () => {
+  return (
+    <nav>
+    <div className="wrapper">
+      
+      <Link
+          href="/home"
+          className="group rounded-lg border border-transparent text-center py-0.3 transition-colors"
+          rel="noopener noreferrer"
+        >
+          <button type="button" className="button">Home</button>
+      </Link>
+
+      <Link
+          href="/orders"
+          className="group rounded-lg border border-transparent text-center py-0.3 transition-colors"
+          rel="noopener noreferrer"
+        >
+          <button type="button" className="button">Orders</button>
+      </Link>
+
+      <Link
+          href="/"
+          className="group rounded-lg border border-transparent text-center py-0.3 transition-colors"
+          rel="noopener noreferrer"
+        >
+          <button type="button" className="button">Products</button>  
+      </Link>
+
+      <Link
+          href="/"
+          className="group rounded-lg border border-transparent text-center py-0.3 transition-colors"
+          rel="noopener noreferrer"
+        >
+        <button type="button" className="button">Customers</button>
+      </Link>
+
+      <Link
+          href="/"
+          className="group rounded-lg border border-transparent text-center py-0.3 transition-colors"
+          rel="noopener noreferrer"
+        >
+        <button type="button" className="button">Admin</button>
+      </Link>
+    </div>
+    </nav>
+  );
+};
+
 const Navbar = () => {
   return (
     <>
@@ -68,4 +117,5 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+// export default Navbar;
+export default NavigationBar;
